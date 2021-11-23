2b285ad0b8872311a82cfc48bb0a9c2bca2c4ef1
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-23 14:23:33 +0100

    update data

--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
 import express from 'express';
 import path from 'path';
-import mongoose from 'mongoose'
-import * as SpeakersController from './controlers/speakersController.js'
+import mongoose from 'mongoose';
+import * as SpeakersController from './controllers/speakersController.js';
 
 mongoose.connect('mongodb://localhost:27017/mongoConference');
-const speakerSchema = mongoose.Schema({
-    name: String,
-    country: String
-});
-const SpeakerModel = mongoose.model("Speaker", speakerSchema);
 
 const app = express();
 const __dirname = path.resolve(path.dirname(''));
@@ -20,27 +15,26 @@ app.set('views', path.join(__dirname, './src/views'));
 app.use(express.static(staticDirectory));
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        pageTitle: "Welcome"
-    });
+	res.render('index', {
+		pageTitle: "Welcome"
+	});
 });
 
 app.get('/speakers', (req, res) => {
-    (async () => {
-        const speakers = await SpeakerModel.find({});
-        res.render('speakers', {
-            pageTitle: "Speakers",
-            speakers
-        });
-    })();
+	(async () => {
+		res.render('speakers', {
+			pageTitle: "Speakers",
+			speakers: await SpeakersController.getAllSpeakers()
+		});
+	})();
 });
- 
+
 app.get('/presentations', (req, res) => {
-    res.render('presentations', {
-        pageTitle: "Presentations"
-    });
+	res.render('presentations', {
+		pageTitle: "Presentations"
+	});
 });
 
 app.listen(port, () => {
-    console.log(`Now listening on port http://localhost:${port}`);
-})
+	console.log(`Now listening on port http://localhost:${port}`);
+});
