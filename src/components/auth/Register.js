997fa7cebd7916990b0ada97a984a6c47018acfe
elis-11 <eliza.arzanukaeva@gmail.com>
2022-01-13 13:16:23 +0100

    register HOOKS

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Register = () => {
-    return (
-        <div>
-            <h1>Register a new account</h1>
-            <form>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <input type="password" placeholder="Verify your password" />
-                <button type="submit">Register</button>
-            </form>
-        </div>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+  const [passwordVerify, setPasswordVerify] = useState("")
 
-export default Register
+  return (
+    <div>
+      <h1>Register a new account</h1>
+      <form>
+        <input 
+        type="email" 
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        />
+
+        <input 
+        type="password" 
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+         />
+
+        <input 
+        type="password" 
+        placeholder="Verify your password"
+        onChange={(e) => setPasswordVerify(e.target.value)}
+        value={passwordVerify}
+         />
+         
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
